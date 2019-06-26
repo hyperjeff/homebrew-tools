07147d2cb01b547f16612f9af79d7c3c26c056f0
Jeff Biggus <hyperjeff@mac.com>
2019-06-25 19:57:02 -0500

    fixed url

--- shuffle.rb
+++ shuffle.rb
@@ -1,7 +1,7 @@
 class Shuffle < Formula
   desc "command-line audio player"
   homepage "https://github.com/hyperjeff/shuffle"
-  url "https://github.com/hyperjeff/shuffle/releases/download/v0.9.1/shuffle-0.9.1.tar.gz"
+  url "https://github.com/hyperjeff/homebrew-tools/releases/download/v0.9.1/shuffle-0.9.1.tar.gz"
   sha256 "8a62bcb10a39f3e32f4ee5fe59146f01833f8c56598881e9706e1de5be29c957"
   version "v0.9.1"
 
