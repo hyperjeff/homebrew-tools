50edb00025a751d7b36a1a41a5df48d53131bf65
Jeff Biggus <hyperjeff@mac.com>
2019-07-12 14:26:25 -0500

    v1.3.0

--- shuffle.rb
+++ shuffle.rb
@@ -3,7 +3,7 @@ class Shuffle < Formula
   homepage "https://github.com/hyperjeff/shuffle"
   url "https://github.com/hyperjeff/homebrew-tools/releases/download/v1.3.0/shuffle-1.3.0.tar.gz"
   sha256 "ac7688e8fb7e9069942bb8f6c700245a0a048eda450b144795c41e49e352b8c7"
-  version "v1.2.0"
+  version "v1.3.0"
 
   bottle :unneeded
 
