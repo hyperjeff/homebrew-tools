2535bba98522c38514ccbae8af635a57581fb60e
Jeff Biggus <hyperjeff@mac.com>
2019-07-12 14:25:19 -0500

    v1.3.0 update

--- shuffle.rb
+++ shuffle.rb
@@ -1,8 +1,8 @@
 class Shuffle < Formula
   desc "command-line audio player"
   homepage "https://github.com/hyperjeff/shuffle"
-  url "https://github.com/hyperjeff/homebrew-tools/releases/download/v1.2.0/shuffle-1.2.0.tar.gz"
-  sha256 "b00bafba9c399084ea5ee2ff1b9b35a9ab4a37285fc267d8dad9b0b6288a7ce4"
+  url "https://github.com/hyperjeff/homebrew-tools/releases/download/v1.3.0/shuffle-1.3.0.tar.gz"
+  sha256 "ac7688e8fb7e9069942bb8f6c700245a0a048eda450b144795c41e49e352b8c7"
   version "v1.2.0"
 
   bottle :unneeded
