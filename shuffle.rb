3ed174111190a8a5c63cd2cab8eb26210bffad70
Jeff Biggus <hyperjeff@mac.com>
2020-06-21 03:24:56 -0500

    update to 1.7.2

--- shuffle.rb
+++ shuffle.rb
@@ -2,7 +2,7 @@ class Shuffle < Formula
   desc "command-line audio player"
   homepage "https://github.com/hyperjeff/shuffle"
   url "https://github.com/hyperjeff/homebrew-tools/releases/download/v1.7.2/shuffle-1.7.2.tar.gz"
-  sha256 "05c9f013c209ce8d63ca2974eada6cdaf86fc1ca108e4ac86a1c4b0a5be5fda7"
+  sha256 "ba381feac8cb016608537d10d6a18c6bf328dff145b68603437be2a8062ab201"
   version "v1.7.2"
 
   bottle :unneeded
