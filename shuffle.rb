9130d5ace62ff33c1404bf9d49de65a498cdaaa7
Jeff Biggus <hyperjeff@mac.com>
2021-11-18 01:44:35 -0600

    update to 1.8.3

--- shuffle.rb
+++ shuffle.rb
@@ -1,9 +1,9 @@
 class Shuffle < Formula
   desc "command-line audio player"
   homepage "https://github.com/hyperjeff/shuffle"
-  url "https://github.com/hyperjeff/homebrew-tools/releases/download/v1.8.2/shuffle-1.8.2.tar.gz"
-  sha256 "5205fb00caa7bc9f90e0ae03172f72249118063bfcc2fabc8ec230b20a88faad"
-  version "v1.8.2"
+  url "https://github.com/hyperjeff/homebrew-tools/releases/download/v1.8.3/shuffle-1.8.3.tar.gz"
+  sha256 "a1e52fdad3f72aa9187cc2eb36e0ef3c23400f9c57117c15cf1254a830880779"
+  version "v1.8.3"
 
   def install
     bin.install "shuffle"
