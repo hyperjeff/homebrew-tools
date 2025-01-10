a92ff209a684457226b8528e83ca73c160bdf750
Jeff Biggus <hyperjeff@mac.com>
2025-01-09 22:03:31 -0600

    Update shuffle.rb
    Updated to 1.9.2 to fix rate flag

--- shuffle.rb
+++ shuffle.rb
@@ -1,9 +1,9 @@
 class Shuffle < Formula
   desc "command-line audio player"
   homepage "https://github.com/hyperjeff/shuffle"
-  url "https://github.com/hyperjeff/homebrew-tools/releases/download/v1.9.1/shuffle-1.9.1.tar.gz"
-  sha256 "a19ebda97328e4482e9f11218a0ab3e5a5bb4385e1bc4eb9d101c33bed7bfdcb"
-  version "v1.9.1"
+  url "https://github.com/hyperjeff/homebrew-tools/releases/download/v1.9.2/shuffle-1.9.2.tar.gz"
+  sha256 "6377efa3d7818e9951fc0ec704a8cd069a7bc041414b4c04f9b994371ac1e8fb"
+  version "v1.9.2"
 
   def install
     bin.install "shuffle"
