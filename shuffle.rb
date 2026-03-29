e93e59d10a98a4f67918f3f26cef3a0dfbd3c76d
Jeff Biggus <hyperjeff@mac.com>
2026-03-28 23:27:27 -0500

    Update Shuffle to version 1.11.0

--- shuffle.rb
+++ shuffle.rb
@@ -1,9 +1,9 @@
 class Shuffle < Formula
   desc "command-line audio player"
   homepage "https://github.com/hyperjeff/shuffle"
-  url "https://github.com/hyperjeff/homebrew-tools/releases/download/v1.10.0/shuffle-1.10.0.tar.gz"
-  sha256 "05d5e399d30dcada768b25545c377529abaa707c8ac6f2c594dd8314f00a0c1e"
-  version "v1.10.0"
+  url "https://github.com/hyperjeff/homebrew-tools/releases/download/v1.11.0/shuffle-1.11.0.tar.gz"
+  sha256 "02bdde6b41c6a3edb281fb409ef0b961b9f4e8aedea96a7e8692cb36e35f3873"
+  version "v1.11.0"
 
   def install
     bin.install "shuffle"
